9e78e37913401f9b8cdb5a267778a77a57c91b83
Ben Berg <ben.berg@buildium.com>
2018-09-08 09:43:50 -0400

    initial commit of serverless function

---
+++ Handler.cs
@@ -0,0 +1,53 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using System;
+using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+
+[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace AwsDotnetCsharp
+{
+    public class Handler  
+    {
+    public APIGatewayProxyResponse Hello(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+      // Log entries show up in CloudWatch
+      context.Logger.LogLine("Example log entry\n");
+
+      var response = new APIGatewayProxyResponse
+      {
+        StatusCode = (int)HttpStatusCode.OK,
+        Body = "{ \"Message\": \"Hello World\" }",
+        Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+      };
+
+      return response;
+    }
+    }
+
+    public class Response
+    {
+      public string Message {get; set;}
+      public Request Request {get; set;}
+
+      public Response(string message, Request request){
+        Message = message;
+        Request = request;
+      }
+    }
+
+    public class Request
+    {
+      public string Key1 {get; set;}
+      public string Key2 {get; set;}
+      public string Key3 {get; set;}
+
+      public Request(string key1, string key2, string key3){
+        Key1 = key1;
+        Key2 = key2;
+        Key3 = key3;
+      }
+    }
+}
